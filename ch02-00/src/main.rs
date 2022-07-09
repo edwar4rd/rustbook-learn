f9ee2e9e6d127941c6b990484c8ef17285af514c
edwar4rd <edwar4rd@gmail.com>
2022-07-09 23:48:26 +0800

    ch02-00: guessing game: loooooop

--- ch02-00/src/main.rs
+++ ch02-00/src/main.rs
@@ -6,21 +6,25 @@ fn main() {
     println!("Guess the number!\n");
 
     let secret_number = rand::thread_rng().gen_range(1..=10);
-    let mut guess = String::new();
-
-    println!("Please input your guess: ");
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    let guess = guess.trim();
-    println!("You guessed: {guess}");
-    let guess: u32 = guess.parse().expect("Unable to parse to number");
-
     println!("The secret number is: {secret_number}");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("too small!"),
-        Ordering::Greater => println!("too big!"),
-        Ordering::Equal => println!("correct!!!"),
+    loop {  
+        println!("Please input your guess: ");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess = guess.trim();
+        println!("You guessed: {guess}");
+        let guess: u32 = guess.parse().expect("Unable to parse to number");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("too small!"),
+            Ordering::Greater => println!("too big!"),
+            Ordering::Equal => {
+                println!("correct!!!");
+                break;
+            },
+        }
     }
 }
