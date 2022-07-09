1eec93c357c7bd750c2336b3c41b15895335f5e9
edwar4rd <edwar4rd@gmail.com>
2022-07-10 00:21:21 +0800

    ch02-00: guessing game: handle invalid input

--- ch02-00/src/main.rs
+++ ch02-00/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     println!("Guess the number!\n");
 
     let secret_number = rand::thread_rng().gen_range(1..=10);
-    println!("The secret number is: {secret_number}");
+    // println!("The secret number is: {secret_number}");
 
     loop {  
         println!("Please input your guess: ");
@@ -15,9 +15,16 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
         let guess = guess.trim();
+        let guess: u32 = match guess.parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("*throw up");
+                println!("*spits out garbage");
+                println!("...");
+                continue;
+            },
+        };
         println!("You guessed: {guess}");
-        let guess: u32 = guess.parse().expect("Unable to parse to number");
-
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("too small!"),
             Ordering::Greater => println!("too big!"),
