dd7e7d3c08ebd074328cdf730f42ebde50d075e5
edwar4rd <edwar4rd@gmail.com>
2022-07-18 00:09:27 +0800

    ch05-03: struct practice: more method

--- ch05-02/src/main.rs
+++ ch05-02/src/main.rs
@@ -1,22 +1,32 @@
-
 #[derive(Debug)]
-struct Vec2ii(i32, i32);
+struct Vec(f32, f32);
+
+impl Vec {
+    fn dist(veca: &Vec, vecb: &Vec)->f32 {
+        ((veca.0-vecb.0)*(veca.0-vecb.0)+(veca.1-vecb.1)*(veca.1-vecb.1)).sqrt()
+    }
+}
+
 
 #[derive(Debug)]
 struct Circle {
-    center: Vec2ii,
-    radius: u32,
+    center: Vec,
+    radius: f32,
 }
 
 impl Circle {
     fn area(&self) -> f32 {
         (self.radius*self.radius) as f32 *3.1415926535898f32
     }
+
+    fn short_dist(&self, point: &Vec) -> f32{
+        Vec::dist(&self.center, &point)-self.radius
+    }
 }
 
 fn main() {
-    let vec1 = Vec2ii(5, 1);
-    let vec2 = Vec2ii(-2, 10);
+    let vec1 = Vec(5.0, 1.0);
+    let vec2 = Vec(-2.0, 10.0);
 
     println!("First vector of the parallelogram: {:?}", vec1);
     println!("Second vector of the parallelogram: {:?}", vec2);
@@ -25,16 +35,16 @@ fn main() {
 
     let ccl1 = Circle{
         center:vec1, 
-        radius:5
+        radius:5.0
     };
     // note after here vec1 is no longer available
 
     println!("circle: {:#?}", ccl1);
     println!("Area of the circle: {}", ccl1.area());
-
+    println!("Distance from vec2 to circle: {}", ccl1.short_dist(&vec2));
     
 }
 
-fn area(veca: &Vec2ii, vecb: &Vec2ii) -> i32 {
+fn area(veca: &Vec, vecb: &Vec) -> f32 {
     veca.0 * vecb.1 - veca.1 * vecb.0
 }
